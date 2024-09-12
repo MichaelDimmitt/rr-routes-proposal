f20259773322de28e0aa08768175baf5a503ca4d
michael <michaelgdimmitt@gmail.com>
2024-09-12 01:58:26 -0400

    narrow down to exit condition, Routes function found in jsx!

--- src/implementations.ts
+++ src/implementations.ts
@@ -1,5 +1,10 @@
 import {createRoutesFromChildren} from 'react-router'
 import {getRouteList} from './reactRoutes';
 import App from '../examples/basic/src/App';
-const list = getRouteList(createRoutesFromChildren(App.default()), "");
-console.log(list);
+function findRoutesList(el) {
+  console.log(el.props.children[2].type.name === 'Routes')
+}
+// const list = getRouteList(createRoutesFromChildren(App.default()), "");
+// @ts-ignore
+const list = findRoutesList(App.default())
+// console.log(list);
